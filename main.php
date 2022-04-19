45dbaf7771df609f4ab9b0d04c3c93e9b2faf48f
ilkatkov <ilkatkov@yandex.ru>
2022-04-19 05:10:12 +0300

    added trees classes

--- main.php
+++ main.php
@@ -19,3 +19,46 @@ class Pear extends Fruit
     protected $min_weight = 130;
     protected $max_weight = 170;
 }
+
+abstract class Tree
+{
+    public function __construct()
+    {
+        $this->id = uniqid();
+        $this->fruits = array();
+
+        $fruits_count = rand($this->min_friuts, $this->max_fruits);
+        for ($i = 0; $i < $fruits_count; $i++) {
+            $this->fruits[] = new $this->fruit;
+        }
+    }
+
+    public function getFruits()
+    {
+        return $this->fruits;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFruitsCount()
+    {
+        return count($this->getFruits());
+    }
+}
+
+class ApplesTree extends Tree
+{
+    protected $min_fruits = 40;
+    protected $max_fruits = 50;
+    protected $fruit = Apple::class;
+}
+
+class PearsTree extends Tree
+{
+    protected $min_fruits = 0;
+    protected $max_fruits = 20;
+    protected $fruit = Pear::class;
+}
